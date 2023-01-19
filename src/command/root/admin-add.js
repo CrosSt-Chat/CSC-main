3bd498bb2cfc57759f6b18745d7c6e9e40660807
Henrize <mail@henrize.kim>
2023-01-19 23:06:04 +0800

    添加增删管理员

---
+++ src/command/root/admin-add.js
@@ -0,0 +1,65 @@
+// 站长添加管理员
+export async function run(hazel, core, hold, socket, data) {
+  // 验证输入的 trip
+  if (!core.verifyTrip(data.trip)) {
+    core.replyWarn('INVALID_TRIP', '请检查您输入的识别码。', socket);
+    return;
+  }
+  
+  // 检查识别码是否已经是管理员
+  if (core.config.adminList.includes(data.trip)) {
+    core.replyWarn('INVALID_TRIP', '请检查您输入的识别码。', socket);
+    return;
+  }
+
+  // 检查识别码是否是成员，如果是则删除
+  if (core.config.memberList.includes(data.trip)) {
+    core.removeFromList(core.config.memberList, data.trip);
+    return;
+  }
+
+  // 添加管理员
+  core.config.adminList.push(data.trip);
+
+  // 保存配置
+  core.saveConfig();
+
+  // 查找管理员的 socket，如果存在则更新权限
+  let matchSockets = core.findSocketTiny('trip', data.trip);
+  if (matchSockets.length > 0) {
+    matchSockets.forEach((matchSocket) => {
+      matchSocket.permission = 'ADMIN';
+      matchSocket.level = core.config.level.admin;
+
+      // 向该管理员发送消息
+      core.replyInfo('PERMISSION_UPDATE', '您的权限已更新。', matchSocket);
+    });
+  }
+
+  // 向全部成员广播消息
+  core.broadcast({
+    cmd: 'info',
+    code: 'ADMIN_ADD',
+    text: '已添加新管理员：' + data.trip,
+    data: { trip: data.trip },
+  }, core.findSocketByLevel(core.config.level.member));
+}
+
+// 用户使用 /addadmin xxxxxx 添加管理员
+export async function execByChat(hazel, core, hold, socket, line) {
+  let trip = line.slice(9).trim();
+
+  // 验证输入的 trip
+  if (!core.verifyTrip(trip)) {
+    core.replyMalformedCommand(socket);
+    return;
+  }
+
+  // 运行命令
+  run(hazel, core, hold, socket, { trip });
+}
+
+export const name = 'addadmin';
+export const requiredLevel = 10;
+export const requiredData = ['trip'];
+export const moduleType = 'ws-command';
---
+++ src/command/root/admin-remove.js
@@ -0,0 +1,59 @@
+// 站长删除管理员
+export async function run(hazel, core, hold, socket, data) {
+  // 验证输入的 trip
+  if (!core.verifyTrip(data.trip)) {
+    core.replyWarn('INVALID_TRIP', '请检查您输入的识别码。', socket);
+    return;
+  }
+  
+  // 检查识别码是否是管理员
+  if (!core.config.adminList.includes(data.trip)) {
+    core.replyWarn('INVALID_TRIP', '请检查您输入的识别码。', socket);
+    return;
+  }
+
+  // 删除管理员
+  core.removeFromList(core.config.adminList, data.trip);
+
+  // 保存配置
+  core.saveConfig();
+
+  // 查找管理员的 socket，如果存在则更新权限
+  let matchSockets = core.findSocketTiny('trip', data.trip);
+  if (matchSockets.length > 0) {
+    matchSockets.forEach((matchSocket) => {
+      matchSocket.permission = 'USER';
+      matchSocket.level = core.config.level.user;
+
+      // 向该管理员发送消息
+      core.replyInfo('PERMISSION_UPDATE', '您的权限已更新。', matchSocket);
+    });
+  }
+
+  // 向全部成员广播消息
+  core.broadcast({
+    cmd: 'info',
+    code: 'ADMIN_ADD',
+    text: '已删除管理员：' + data.trip,
+    data: { trip: data.trip },
+  }, core.findSocketByLevel(core.config.level.member));
+}
+
+// 用户使用 /deladmin xxxxxx 添加管理员
+export async function execByChat(hazel, core, hold, socket, line) {
+  let trip = line.slice(9).trim();
+
+  // 验证输入的 trip
+  if (!core.verifyTrip(trip)) {
+    core.replyMalformedCommand(socket);
+    return;
+  }
+
+  // 运行命令
+  run(hazel, core, hold, socket, { trip });
+}
+
+export const name = 'deladmin';
+export const requiredLevel = 10;
+export const requiredData = ['trip'];
+export const moduleType = 'ws-command';
