7afec19d439adfa9c6004316de1db15c36978559
Henrize <mail@henrize.kim>
2023-01-19 22:46:21 +0800

    添加 member-remove.js

---
+++ src/command/admin/member-remove.js
@@ -0,0 +1,56 @@
+// 管理员删除成员
+export async function run(hazel, core, hold, socket, data) {
+  // 验证输入的 trip
+  if (!core.verifyTrip(data.trip)) {
+    core.replyWarn('INVALID_TRIP', '请检查您输入的识别码。', socket);
+    return;
+  }
+
+  // 检查识别码是否是成员
+  if (!core.config.memberList.includes(data.trip)) {
+    core.replyWarn('INVALID_TRIP', '请检查您输入的识别码。', socket);
+    return;
+  }
+
+  // 检查识别码是否已经是管理员
+  if (core.config.adminList.includes(data.trip)) {
+    core.replyWarn('PERMISSION_DENIED', '越权操作。', socket);
+    return;
+  }
+
+  // 删除成员
+  core.removeFromArray(core.config.memberList, data.trip);
+
+  // 保存配置
+  core.saveConfig();
+
+  // 查找成员的 socket，如果存在则更新权限
+  let matchSockets = core.findSocketTiny('trip', data.trip);
+  if (matchSockets.length > 0) {
+    matchSockets.forEach((matchSocket) => {
+      matchSocket.permission = 'USER';
+      matchSocket.level = core.config.level.user;
+
+      // 向成员发送消息
+      core.replyInfo('PERMISSION_UPDATE', '您的权限已更新。', matchSocket);
+    });
+  }
+
+  // 向全部成员广播消息
+  core.broadcast({
+    cmd: 'info',
+    code: 'MEMBER_REMOVE',
+    text: '已删除成员：' + data.trip,
+    data: { trip: data.trip },
+  }, core.findSocketByLevel(core.config.level.member));
+}
+
+// 用户使用 /delmem xxxxxx 删除成员
+export async function execByChat(hazel, core, hold, socket, line) {
+  this.run(hazel, core, hold, socket, { trip: line.slice(7).trim() });
+}
+
+export const name = 'delmem';
+export const requiredLevel = 4;
+export const requiredData = ['trip'];
+export const moduleType = 'ws-command';
