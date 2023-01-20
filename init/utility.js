1163112912b5626baa632e407ada2760875c086b
Henrize <mail@henrize.kim>
2023-01-20 21:01:34 +0800

    添加用于处理命令的函数

--- init/utility.js
+++ init/utility.js
@@ -1,26 +1,34 @@
 // 各种不知道在哪能用到的工具函数
-
-export async function run( hazel, core, hold ) {
+export async function run(hazel, core, hold) {
   // 净化对象以防止原型链污染
-  core.purifyObject = function ( input ) {
-    let output = Object.create( null );
-    for ( let objectKey in input ) {
-      if ( objectKey != '__proto__' && objectKey != 'constructor' && objectKey != 'prototype' ) {
-        output[ objectKey ] = input[ objectKey ];
+  core.purifyObject = function (input) {
+    let output = Object.create(null);
+    for (let objectKey in input) {
+      if (objectKey != '__proto__' && objectKey != 'constructor' && objectKey != 'prototype') {
+        output[objectKey] = input[objectKey];
       }
     }
     return output;
   }
 
   // 从数组中删除指定元素
-  core.removeFromArray = function ( array, element ) {
-    let index = array.indexOf( element );
-    if ( index > -1 ) {
-      array.splice( index, 1 );
+  core.removeFromArray = function (array, element) {
+    let index = array.indexOf(element);
+    if (index > -1) {
+      array.splice(index, 1);
       return true;
     } else {
       return false;
     }
+  }
+
+  // 拆分字符串中的参数
+  core.splitArgs = function (line) {
+    let args = [];
+    line.split(' ').forEach((arg) => {
+      if (arg.trim() != '') args.push(arg);
+    });
+    return args;
   }
 
   // 获取就像 [20:42:13] 一样的时间字符串
