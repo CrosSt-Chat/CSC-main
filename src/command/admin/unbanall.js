1b45103d3259e4ed0f9d7d3f43bc9c6340879fba
Henrize <mail@henrize.kim>
2023-01-20 19:15:16 +0800

    添加 unbanall.js

---
+++ src/command/admin/unbanall.js
@@ -0,0 +1,26 @@
+// 解封全部 IP 地址
+export async function run(hazel, core, hold, socket, data) {
+  // 解封该 IP 地址
+  hold.bannedIPlist = [];
+
+  // 通知全部管理员
+  core.broadcastInfo(
+    'UNBAN_ALL',
+    socket.nick + ' 解封了全部 IP 地址。',
+    core.findSocketByLevel(4),
+    { from: socket.nick }
+  );
+
+  // 写入存档
+  core.archive('UBA', socket, '');
+}
+
+// 通过 /unbanall 命令解封全部 IP 地址
+export async function execByChat(hazel, core, hold, socket, line) {
+  await run(hazel, core, hold, socket);
+}
+
+export const name = 'unbanall';
+export const requiredLevel = 4;
+export const requiredData = ['address'];
+export const moduleType = 'ws-command';
